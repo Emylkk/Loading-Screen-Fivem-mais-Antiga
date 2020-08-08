b58c5f2e8b900249bd43d8f0c98717c222947f3a
Emerson R. S. A <33014935+Emylkk@users.noreply.github.com>
2020-08-08 04:04:13 -0300

    Add files via upload

---
+++ vrp_loading/__resource.lua
@@ -0,0 +1,21 @@
+files {
+    'index.html',
+    'hungria.mp3',
+    'remixyoualreadyknow.mp3',
+    'img/fundo.jpg',
+    'img/fundo2.jpg',
+    'img/fundo3.png',
+    'img/logo-light.png',
+    'img/cursor.png',
+    'img/icon-fire.png',
+    'img/icon-regraspng.png',
+    'img/icon-terno.png',
+    'img/icon-ternobranco.png',
+    'js/menus.js',
+    'css/style.css'
+
+}
+
+loadscreen 'index.html'
+
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
